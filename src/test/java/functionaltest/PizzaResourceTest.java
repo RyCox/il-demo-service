a238dd8fb7b426b8fd0158381634984bdb4b5c53
Ryan Cox <ryan.cox.312@gmail.com>
2019-03-23 19:09:41 -0400

    Added test in pizza functional test.

--- src/test/java/functionaltest/PizzaResourceTest.java
+++ src/test/java/functionaltest/PizzaResourceTest.java
@@ -1,12 +1,24 @@
 package functionaltest;
 
+import com.rycox.ildemo.domain.Pizza;
+import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class PizzaResourceTest extends RestAssuredTest {
 
     @Test
     public void testGetPizza() {
+        Response pizzaResponse = given().get("/pizza");
+        Assert.assertEquals(200, pizzaResponse.getStatusCode());
 
+        Pizza pizzaObject = pizzaResponse.as(Pizza.class);
+        Assert.assertTrue(pizzaObject.getIngredientList().contains("Cheese"));
+        Assert.assertTrue(pizzaObject.getIngredientList().contains("Sauce"));
+        Assert.assertTrue(pizzaObject.getIngredientList().contains("Dough"));
+        Assert.assertTrue(pizzaObject.getIngredientList().contains("Pepperoni"));
     }
 
     @Test
